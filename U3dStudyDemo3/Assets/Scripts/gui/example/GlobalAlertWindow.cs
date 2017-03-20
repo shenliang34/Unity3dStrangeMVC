cbd88e0606a360b41266cc6a76feae513226f288
shenliangliang <shenliangliang@gamehero.me>
2017-03-20 20:40:53 +0800

    *优化

--- U3dStudyDemo3/Assets/Scripts/gui/example/GlobalAlertWindow.cs
+++ U3dStudyDemo3/Assets/Scripts/gui/example/GlobalAlertWindow.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GlobalAlertWindow : BaseWindow
 {
-
     // Use this for initialization
+    private Button centerBtn;
+    private Button leftBtn;
+    private Button rightBtn;
+    private Text alert;
     
     protected override void Start()
     {
         base.Start();
+
+        
     }
 
     protected override void OnGUI()
--- U3dStudyDemo3/Assets/Scripts/gui/main/manager/WindowManager.cs
+++ U3dStudyDemo3/Assets/Scripts/gui/main/manager/WindowManager.cs
@@ -6,7 +6,7 @@ public class WindowManager : MonoBehaviour
 
     // Use this for initialization
     private TestWidnow testwindow;
-
+    
     void Start()
     {
 
